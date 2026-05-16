d1843f5801b1645270f5010d313f8332f568e788
Nayan Ranjan Das <nayanranjandas777@gmail.com>
2026-05-16 12:15:37 +0530

    fix auth middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -9,14 +9,14 @@ const protect = async (req, res, next) => {
       token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decoded.id).select("-password");
-      next();
+      return next();
     } catch(error){
-      res.status(401).json({ message: "Not authorized" });
+      return res.status(401).json({ message: "Not authorized" });
     }
   }
 
   if(!token){
-    res.status(401).json({ message: "No token" });
+    return res.status(401).json({ message: "No token" });
   }
 };
 
